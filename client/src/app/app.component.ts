f6ee6609b916a22eeffe3266cff0eba8d6996730
00charan00 <poycharan@gmail.com>
2024-12-03 09:30:05 +0530

    header footer bug cleared

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,RouterOutlet,FooterComponent],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
-  
+// export class AppComponent {
+//   title = 'client';
+//
+// }
+export class AppComponent implements OnInit {
+  showHeaderFooter: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      const currentUrl = this.router.url;
+      this.showHeaderFooter = !(currentUrl === '/register' || currentUrl === '/signup'||currentUrl==='/login');
+    });
+  }
 }
