40468e7770ce0f35b46c2499124b6c08c8040ff9
muthu <mutheeslavanya@gmail.com>
2024-12-09 12:34:01 +0530

    meeting page over

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,15 +8,18 @@ import {EventsComponent} from './events/events.component';
 import {AdminGuard} from './guards/admin.guard';
 import {AuthGuard} from './guards/auth.guard';
 import {EmpCrudComponent} from './emp-crud/emp-crud.component';
+import {MeetingComponent} from './meeting/meeting.component';
 
 export const routes: Routes = [
   { path: '', component: EmpHomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'meeting', component: MeetingComponent },
   { path: 'empcrud', component: EmpCrudComponent },
   { path: 'events', component: EventsComponent , canActivate:[AuthGuard]},
   { path: 'admin', component: AdminHomeComponent,canActivate:[AdminGuard] },  // Protect admin route
   { path: 'employee', component: EmpHomeComponent },  // Protect employee route
   { path: '**', redirectTo: '' }
+
 ];
